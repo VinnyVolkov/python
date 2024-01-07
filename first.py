7f7910f3a5c5f96c6a0162b5deb119e4d742a556
VinnyVolkov <yura.volkov061003@gmail.com>
2024-01-07 15:11:00 +0200

    Власник

--- first.py
+++ first.py
@@ -1,2 +1,13 @@
-print("hello")
-print("hello")
+#print("hello")
+#print("hello")
+#print("Hello", end=" next")
+
+#a = input("Рядок запрошення: ")
+#
+#  На екрані ви побачите: Рядок запрошення: #
+
+a = input("Хто ти: ")
+if a=="Yura":
+ print('Привіт Власник')
+else:
+ print(f"Привіт, {a}!")
