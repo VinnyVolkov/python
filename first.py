440e3a625b81d9a84bc806ddb5e681fd692621dd
VinnyVolkov <yura.volkov061003@gmail.com>
2024-01-08 13:54:31 +0200

    Count()

--- first.py
+++ first.py
@@ -18,26 +18,30 @@
 # P= a*4
 # print(f"Периметир квадрата:{P}")
 
-
+# Coffee
 # Встановлюємо ціни на продукти
-price_per_croissant = 1.04
-price_per_glass = 0.34
-price_per_coffee_pack = 4.42
+# price_per_croissant = 1.04
+# price_per_glass = 0.34
+# price_per_coffee_pack = 4.42
 
-# Кількість кожного продукту
-num_croissants = int(input("Введіть кількість круасанів: "))
-num_glasses = int(input("Введіть кількість склянок: "))
-num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
+# # Кількість кожного продукту
+# num_croissants = int(input("Введіть кількість круасанів: "))
+# num_glasses = int(input("Введіть кількість склянок: "))
+# num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
 
-# Обчислення загальної вартості
-total_cost = num_croissants * price_per_croissant + \\
-             num_glasses * price_per_glass + \\
-             num_coffee_packs * price_per_coffee_pack
+# # Обчислення загальної вартості
+# total_cost = num_croissants * price_per_croissant+ \
+#              num_glasses * price_per_glass+ \
+#              num_coffee_packs * price_per_coffee_pack
 
-# Визначаємо кількість повних доларів і центів
-total_dollars = int(total_cost)
-total_cents = int(total_cost * 100)
+# # Визначаємо кількість повних доларів і центів
+# total_dollars = int(total_cost)
+# total_cents = int(total_cost * 100)
 
-# Вивід результату
-print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
-print(f"Загальна вартість у центах: {total_cents} центів")
+# # Вивід результату
+# print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
+# print(f"Загальна вартість у центах: {total_cents} центів")
+
+my_list = [1, 2, 3, 4, 2, 2, 5, 2, 4]
+count_2 = my_list.count(4)
+print(count_2)  # Виведе 4, оскільки число 2 зустрічається 4 рази
