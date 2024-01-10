5b41fd27f45f351b912a86223356becacf5f8c87
VinnyVolkov <yura.volkov061003@gmail.com>
2024-01-10 16:06:55 +0200

    Format

--- first.py
+++ first.py
@@ -67,3 +67,18 @@
 # print(intersection)  # frozenset({3})
 # print(difference)  # frozenset({1, 2})
 # print(symmetric_difference)  # frozenset({1, 2, 4, 5})
+
+# #.format
+# # Просте форматування рядка
+# name = 'John'
+# print('Hello, {}!'.format(name))
+
+# # Форматування з декількома аргументами
+# age = 25
+# print('Hello, {}. You are {} years old.'.format(name, age))
+
+# # Використання іменованих аргументів
+# print('Hello, {name}. You are {age} years old.'.format(name='Jane', age=30))
+
+# # Використання індексів для вказівки порядку аргументів
+# print('Hello, {1}. You are {0} years old.'.format(age, name))
