7edb9736bfe0e259e77cc79605278c3c358e3771
VinnyVolkov <yura.volkov061003@gmail.com>
2024-01-15 14:34:08 +0200

    zip

--- first.py
+++ first.py
@@ -244,3 +244,8 @@
 # some_list = ["apple", "banana", "cherry"]
 # for index, value in enumerate(some_list):
 #     print(index, value)
+
+# list1 = ["зелене", "стигла", "червоний"]
+# list2 = ["яблуко", "вишня", "томат"]
+# for number, letter in zip(list1, list2):
+#     print(number, letter)
