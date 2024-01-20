95c10941beeb536eab4a0b970f9809aa101c98c2
VinnyVolkov <yura.volkov061003@gmail.com>
2024-01-20 22:10:28 +0200

    az_sring

--- urok.py
+++ urok.py
@@ -26,3 +26,47 @@
 #     print("Число x є парним.")
 # else:
 #     print("Число x є непарним.")
+
+# a = 5
+# b = 1
+# max_value = a if a > b else b
+# print(max_value)
+
+# num = int(input("Число "))
+# sum = 0
+
+# while num >= 1:
+#       sum += num
+#       num -= 1
+# print(sum)
+
+# list = [1,5,3,4,6,7]
+# sum=0
+# for i in list:
+#     print(i)
+#     sum+=i
+# print(sum)
+
+
+# length = int(20)
+# width = int(15)
+# area = length * width
+# area = int(area)
+# print(area)
+
+
+# celsius = float(input("Введіть температуру в градусах Цельсія: "))
+# fahrenheit = (celsius * 9/5) + 32
+
+# print(f"{celsius} градусів Цельсія дорівнює {fahrenheit:.2f} градусам Фаренгейта.")
+
+# val = str(input("Ведіть слово "))
+# k= len(val)
+# print(f" Кількість символів в даному рядкустановить {k} ")
+
+# is_palindrome = input(" Ведіть ваше слово ")
+# reverse = is_palindrome
+# if reverse == is_palindrome [::-1]:
+#     print("Слово є палідромом")
+# else:
+#     print(" Нажаль слово не є палідромом!")
